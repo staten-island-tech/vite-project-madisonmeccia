b208d5d2d9343813bffa399928e2d182c02101a8
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2022-12-19 01:25:37 -0500

    d

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,8 @@
 import "../styles/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
 import { menu } from "./menu";
