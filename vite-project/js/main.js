581dc6acfa18365a8d2694b059697833ebeec38f
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2022-12-19 02:24:29 -0500

    b

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -26,3 +26,12 @@ document.querySelector("#app").innerHTML = `
 
 setupCounter(document.querySelector("#counter"));
 console.log(menu);
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
