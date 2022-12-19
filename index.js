1c558f3c74a0fa0e321bbb76a54a9e274a083965
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2022-12-18 22:01:33 -0500

    n

---
+++ index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import { menu } from "./menu";
+document.querySelector("#app").innerHTML = `
+  <h1>Hello?????</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+console.log(menu);
