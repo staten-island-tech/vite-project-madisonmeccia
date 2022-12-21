a124fb6891ae660ad0b0c8cead3794815867b505
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2022-12-20 19:24:28 -0500

    d

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,16 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
 import { menu } from "./menu";
-document.querySelector("#app").innerHTML = `
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
 console.log(menu);
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
