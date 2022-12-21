fd86e3ce70b74f1edccdc1c0c1bd16f77ec4682b
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2022-12-20 20:30:05 -0500

    d

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,9 @@
-import "../styles/style.css";
+/*import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
-import { menu } from "./menu";
+import { menu } from "./menu.js";
 console.log(menu);
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
@@ -13,4 +13,134 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.body.classList.add("light");
     document.body.classList.remove("dark");
   }
+}); */
+
+const window = {
+  view: document.getElementById("view"),
+  bar: document.querySelector(".bar"),
+  tab: document.querySelectorAll(".tab"),
+  menu: document.querySelector(".menu"),
+  logoPic: document.querySelector(".logoPic"),
+  food: document.querySelectorAll(".food"),
+  meatTitle: document.querySelector(".meatTitle"),
+  meatOption: document.querySelectorAll(".selectopt"),
+  meatSelect: document.querySelector('input[name="test"]:checked').value,
+  root: document.querySelector(":root"),
+  showVeg: document.querySelector(".showVeg"),
+  showFull: document.querySelector(".showFull"),
+};
+
+window.view.addEventListener("click", function () {
+  if (this.style.backgroundImage == 'url("../images/sun.png")') {
+    this.style.backgroundImage = "url('../images/moon.png')";
+    window.logoPic.src = "../images/logo-blue.png";
+
+    document.body.classList = "dark";
+    theme = "foodDark";
+    createMenu();
+  } else {
+    this.style.backgroundImage = 'url("../images/sun.png")';
+    window.logoPic.src = "../images/logo-red.png";
+
+    document.body.classList = ["light"];
+    theme = "foodLight";
+    createMenu();
+  }
 });
+
+document.body.onload = function () {
+  window.view.style.backgroundImage = "url('../images/sun.png')";
+  createMenu();
+  update();
+};
+
+window.showVeg.addEventListener("click", function () {
+  sortMenu("no meat");
+});
+
+window.meatOption.forEach((input) =>
+  input.addEventListener("click", function () {
+    if (this.value == "all") {
+      createMenu();
+    } else {
+      sortMenu(this.value);
+    }
+  })
+);
+
+window.showFull.addEventListener("click", function () {
+  deals("buttonPress");
+});
+
+setInterval(update, 10000);
+
+function update() {
+  console.log("running");
+  let date = new Date();
+  if (dayToday != date.getDay()) {
+    dayToday = date.getDay();
+    dailyMenu = menu.filter((item) => item.days.includes(dayToday));
+    createDayMenu();
+  }
+}
+function deals(change) {
+  if (change == "buttonPress") {
+    if (window.showFull.innerHTML == "Show Full Menu") {
+      window.showFull.innerHTML = "Show Daily Deals";
+      createMenu();
+    } else {
+      window.showFull.innerHTML = "Show Full Menu";
+      createDayMenu();
+    }
+  } else {
+    createDayMenu();
+  }
+}
+
+function createDayMenu() {
+  console.log(1);
+
+  window.menu.replaceChildren();
+  dailyMenu.forEach((item) => {
+    window.menu.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="food ${theme}">
+        <p1 class="name">${item.dish}</p1>
+        <p2 class="description">${item.description}</p2>
+        <strike>$${item.price}</strike>
+        <p3 class="price">$ ${item.price / 2}</p3>
+        <img src="${item.image}" class="foodImg">`
+    );
+  });
+}
+
+function createMenu() {
+  window.menu.replaceChildren();
+
+  menu.forEach((item) => {
+    window.menu.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="food ${theme}">
+        <p1 class="name">${item.dish}</p1>
+        <p2 class="description">${item.description}</p2>
+        <p3 class="price">$${item.price}</p3>
+        <img src="${item.image}" class="foodImg">`
+    );
+  });
+}
+
+function sortMenu(meatType) {
+  let sortedMenu = menu.filter((meat) => meat.meat.includes(meatType));
+
+  window.menu.replaceChildren();
+  sortedMenu.forEach((food) => {
+    window.menu.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="food ${theme}">
+        <p1 class="name">${food.dish}</p1>
+        <p2 class="description">${food.description}</p2>
+        <p3 class="price">$${food.price}</p3>
+        <img src="${food.image}" class="foodImg">`
+    );
+  });
+}
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,59 +1,88 @@
 const menu = [
   {
-    name: "Pizza Slice",
-    vegetarian: true,
-    price: 2.0,
-    img: "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    inStock: true,
+    dish: "Shashlik",
+    price: 13.66,
+    description: "Steak and chicken cooked over an open fire on a shishkabob.",
+    image:
+      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Shashlik.jpg?w=1200&ssl=1",
+    meat: ["beef", "chicken"],
+    days: [3, 6, 7],
   },
   {
-    name: "Salad",
-    vegetarian: true,
-    price: 5.0,
-    img: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    inStock: true,
+    dish: "Schi",
+    price: 3.28,
+    description:
+      "Fresh or fermented cabbage, with potatoes, carrots, and onions.",
+    image:
+      "https://www.tripsavvy.com/thmb/YvS-R2iQauDfDoGE1e0efclFO8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-556668045-5b953f7b46e0fb0050af9cce.jpg",
+    meat: ["no meat"],
+    days: [2, 5, 7],
   },
   {
-    name: "Soup",
-    vegetarian: true,
-    price: 3.0,
-    img: "https://images.unsplash.com/photo-1547592166-23ac45744acd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
-    inStock: true,
+    dish: "Pirozhki",
+    price: 4.32,
+    description: "A fried puff pastry made with cabbage, potatoes and cheese.",
+    image:
+      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Pirozhki-Puff-Pastry-with-Meat.jpg?w=1200&ssl=1",
+    meat: ["no meat"],
+    days: [1, 5, 7],
   },
   {
-    name: "Grilled Cheese",
-    vegetarian: true,
-    price: 4,
-    img: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
-    inStock: false,
+    dish: "Pelmeni",
+    price: 5.26,
+    description:
+      "Dumplings that have a dough surounding and a meat filling, served in chicken broth with a side of sour cream.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnpFU3TO1p13q0UZvNR9ttSN9Vq4i5Aj3Fkg&usqp=CAU",
+    meat: ["lamb", "chicken", "beef"],
+    days: [1, 2, 4],
   },
   {
-    name: "Roast Chicken",
-    vegetarian: false,
-    price: 8.0,
-    img: "https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    inStock: true,
+    dish: "Cutlet with Buckwheat",
+    price: 4.14,
+    description: "A classic russian cutlet with buckwheat.",
+    image: "https://i.ytimg.com/vi/97rL-AY5vqA/maxresdefault.jpg",
+    meat: ["chicken", "beef"],
+    days: [1, 3, 7],
   },
   {
-    name: "Steak",
-    vegetarian: false,
-    price: 10.0,
-    img: "https://images.unsplash.com/photo-1574969884448-fe5bce3d0d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80",
-    inStock: false,
+    dish: "Bubliki",
+    price: 1.82,
+    description:
+      "A ring of yeast-leavened wheat dough, that has been boiled in water for a short time before baking.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Bublik_in_Kiev_with_Sesame.JPG/123px-Bublik_in_Kiev_with_Sesame.JPG",
+    meat: ["no meat"],
+    days: [4, 5, 7],
   },
   {
-    name: "Cheeseburger",
-    vegetarian: false,
-    price: 6,
-    img: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=902&q=80",
-    inStock: true,
+    dish: "Borscht",
+    price: 7.56,
+    description: "A beet soup made with a variety of vegetables and beef.",
+    image:
+      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Borscht-russian-food.jpg?w=1112&ssl=1",
+    meat: ["beef"],
+    days: [3, 4, 6],
   },
   {
-    name: "Ice Cream",
-    vegetarian: false,
-    price: 3.0,
-    img: "https://images.unsplash.com/photo-1566454419290-57a64afe30ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-    inStock: true,
+    dish: "Blini",
+    price: 5.84,
+    description:
+      "Very thin and sweet pancakes that can be rolled up with any filling of your choice, typically honey or jam.",
+    image:
+      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/blini-russian-pancakes.jpg?w=910&ssl=1",
+    meat: ["chicken"],
+    days: [1, 4, 7],
+  },
+  {
+    dish: "Beef Stroganoff",
+    price: 10.44,
+    description: "Sauteed beef served with some rice and vegetables.",
+    image:
+      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Beef-Stroganoff.jpg?w=1200&ssl=1",
+    meat: ["beef"],
+    days: [2, 3, 6],
   },
 ];
+
 export { menu };
