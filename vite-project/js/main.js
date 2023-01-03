011a215be5882a20eb9cefea7bd8f1151325656e
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-01-02 23:24:59 -0500

    d

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,61 +1,30 @@
-/*import "../styles/style.css";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.init();
 import { menu } from "./menu.js";
-console.log(menu);
-document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("light")) {
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
-  } else {
-    document.body.classList.add("light");
-    document.body.classList.remove("dark");
-  }
-}); */
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
+
+let theme = "light",
+  dayToday = 0,
+  dailyMenu = 0;
 
 const window = {
-  view: document.getElementById("view"),
-  bar: document.querySelector(".bar"),
-  tab: document.querySelectorAll(".tab"),
   menu: document.querySelector(".menu"),
-  logoPic: document.querySelector(".logoPic"),
   food: document.querySelectorAll(".food"),
-  meatTitle: document.querySelector(".meatTitle"),
   meatOption: document.querySelectorAll(".selectopt"),
   meatSelect: document.querySelector('input[name="test"]:checked').value,
   root: document.querySelector(":root"),
-  showVeg: document.querySelector(".showVeg"),
-  showFull: document.querySelector(".showFull"),
+  showNomeat: document.querySelector(".showNomeat"),
+  showAll: document.querySelector(".showAll"),
 };
 
-window.view.addEventListener("click", function () {
-  if (this.style.backgroundImage == 'url("../images/sun.png")') {
-    this.style.backgroundImage = "url('../images/moon.png')";
-    window.logoPic.src = "../images/logo-blue.png";
-
-    document.body.classList = "dark";
-    theme = "foodDark";
-    createMenu();
-  } else {
-    this.style.backgroundImage = 'url("../images/sun.png")';
-    window.logoPic.src = "../images/logo-red.png";
-
-    document.body.classList = ["light"];
-    theme = "foodLight";
-    createMenu();
-  }
-});
-
 document.body.onload = function () {
-  window.view.style.backgroundImage = "url('../images/sun.png')";
   createMenu();
   update();
 };
 
-window.showVeg.addEventListener("click", function () {
-  sortMenu("no meat");
+window.showNomeat.addEventListener("click", function () {
+  sortMenu("n/a");
 });
 
 window.meatOption.forEach((input) =>
@@ -68,11 +37,9 @@ window.meatOption.forEach((input) =>
   })
 );
 
-window.showFull.addEventListener("click", function () {
+window.showAll.addEventListener("click", function () {
   deals("buttonPress");
 });
-
-setInterval(update, 10000);
 
 function update() {
   console.log("running");
@@ -85,11 +52,11 @@ function update() {
 }
 function deals(change) {
   if (change == "buttonPress") {
-    if (window.showFull.innerHTML == "Show Full Menu") {
-      window.showFull.innerHTML = "Show Daily Deals";
+    if (window.showAll.innerHTML == "complete menu") {
+      window.showAll.innerHTML = "half price!";
       createMenu();
     } else {
-      window.showFull.innerHTML = "Show Full Menu";
+      window.showAll.innerHTML = "complete menu";
       createDayMenu();
     }
   } else {
@@ -104,7 +71,7 @@ function createDayMenu() {
   dailyMenu.forEach((item) => {
     window.menu.insertAdjacentHTML(
       "afterbegin",
-      `<div class="food ${theme}">
+      `<div class="food ${item.theme}">
         <p1 class="name">${item.dish}</p1>
         <p2 class="description">${item.description}</p2>
         <strike>$${item.price}</strike>
@@ -136,7 +103,7 @@ function sortMenu(meatType) {
   sortedMenu.forEach((food) => {
     window.menu.insertAdjacentHTML(
       "afterbegin",
-      `<div class="food ${theme}">
+      `<div class="food" ${food.theme}>
         <p1 class="name">${food.dish}</p1>
         <p2 class="description">${food.description}</p2>
         <p3 class="price">$${food.price}</p3>
@@ -144,3 +111,12 @@ function sortMenu(meatType) {
     );
   });
 }
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,87 +1,76 @@
 const menu = [
   {
-    dish: "Shashlik",
-    price: 13.66,
-    description: "Steak and chicken cooked over an open fire on a shishkabob.",
-    image:
-      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Shashlik.jpg?w=1200&ssl=1",
-    meat: ["beef", "chicken"],
+    dish: "penne with vodka",
+    image: "./images/penne.jpg",
+    description: "penne pasta with vodka sauce",
+    price: 23.69,
+    meat: ["n/a"],
     days: [3, 6, 7],
   },
   {
-    dish: "Schi",
-    price: 3.28,
-    description:
-      "Fresh or fermented cabbage, with potatoes, carrots, and onions.",
-    image:
-      "https://www.tripsavvy.com/thmb/YvS-R2iQauDfDoGE1e0efclFO8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-556668045-5b953f7b46e0fb0050af9cce.jpg",
-    meat: ["no meat"],
-    days: [2, 5, 7],
+    dish: "chicken francese",
+    image: "./images/chicken.jpeg",
+    description: "chicken with lemon and a side of pasta",
+    price: 27.92,
+    meat: ["chicken"],
+    days: [1, 2, 7],
   },
   {
-    dish: "Pirozhki",
-    price: 4.32,
-    description: "A fried puff pastry made with cabbage, potatoes and cheese.",
-    image:
-      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Pirozhki-Puff-Pastry-with-Meat.jpg?w=1200&ssl=1",
-    meat: ["no meat"],
-    days: [1, 5, 7],
-  },
-  {
-    dish: "Pelmeni",
-    price: 5.26,
-    description:
-      "Dumplings that have a dough surounding and a meat filling, served in chicken broth with a side of sour cream.",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnpFU3TO1p13q0UZvNR9ttSN9Vq4i5Aj3Fkg&usqp=CAU",
-    meat: ["lamb", "chicken", "beef"],
-    days: [1, 2, 4],
-  },
-  {
-    dish: "Cutlet with Buckwheat",
-    price: 4.14,
-    description: "A classic russian cutlet with buckwheat.",
-    image: "https://i.ytimg.com/vi/97rL-AY5vqA/maxresdefault.jpg",
-    meat: ["chicken", "beef"],
-    days: [1, 3, 7],
-  },
-  {
-    dish: "Bubliki",
-    price: 1.82,
-    description:
-      "A ring of yeast-leavened wheat dough, that has been boiled in water for a short time before baking.",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Bublik_in_Kiev_with_Sesame.JPG/123px-Bublik_in_Kiev_with_Sesame.JPG",
-    meat: ["no meat"],
-    days: [4, 5, 7],
-  },
-  {
-    dish: "Borscht",
-    price: 7.56,
-    description: "A beet soup made with a variety of vegetables and beef.",
-    image:
-      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Borscht-russian-food.jpg?w=1112&ssl=1",
+    dish: "pasta with sunday sauce",
+    image: "./images/pasta.jpg",
+    description: "pasta with fresh tomato sauce, chopmeat, meatballs",
+    price: 20.87,
     meat: ["beef"],
+    days: [4, 5, 6],
+  },
+  {
+    dish: "brick oven pizza",
+    image: "./images/pizza1.jpg",
+    description: "brick oven vodka sauce pizza",
+    price: 5.65,
+    meat: ["beef"],
+    days: [5, 6, 7],
+  },
+  {
+    dish: "brick oven pizza",
+    price: 4.92,
+    description: "brick oven tomato sauce pizza",
+    image: "./images/pizza2.jpg",
+    meat: ["fish", "pepperoni"],
+    days: [2, 3, 5],
+  },
+  {
+    dish: "penne with bolognese",
+    image: "./images/pizza3.jpg",
+    description: "penne with a beef and tomato sauce",
+    price: 54.32,
+    meat: ["beef"],
+    days: [3, 4, 7],
+  },
+  {
+    dish: "linguini with clam sauce",
+    image: "./images/pizza4.jpeg",
+    description: "linguini with clams and sauce",
+    price: 75.2,
+    meat: ["fish"],
     days: [3, 4, 6],
   },
+
   {
-    dish: "Blini",
-    price: 5.84,
-    description:
-      "Very thin and sweet pancakes that can be rolled up with any filling of your choice, typically honey or jam.",
-    image:
-      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/blini-russian-pancakes.jpg?w=910&ssl=1",
-    meat: ["chicken"],
-    days: [1, 4, 7],
+    dish: "eggplant parmigiana",
+    image: "./images/pizza5.jpg",
+    description: "baked eggplan, sauce, cheese",
+    price: 40.0,
+    meat: ["n/a"],
+    days: [3, 4, 5],
   },
   {
-    dish: "Beef Stroganoff",
-    price: 10.44,
-    description: "Sauteed beef served with some rice and vegetables.",
-    image:
-      "https://i0.wp.com/www.trafalgar.com/real-word/wp-content/uploads/sites/3/2020/02/Beef-Stroganoff.jpg?w=1200&ssl=1",
-    meat: ["beef"],
-    days: [2, 3, 6],
+    dish: "tortellini with creamy sauce",
+    image: "./images/pizza6.jpg",
+    description: "tortellini, creamy sauce, basil",
+    price: 37.56,
+    meat: ["n/a"],
+    days: [2, 5, 6],
   },
 ];
 
